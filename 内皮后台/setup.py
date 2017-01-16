bb04c16be81ba6c69b8607a54c2ed9bcc0f51fcc
DESKTOP-8RMKUQ8\11710 <1171067930@qq.com>
2017-01-16 15:02:48 +0800

    no message

---
+++ 内皮后台/setup.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 17:20:46 2017
+
+@author: 11710
+"""
+
+from distutils.core import setup
+import py2exe
+
+setup(console=['gui.py'])
---
+++ 内皮后台/test.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sat Jan 14 16:17:13 2017
+
+@author: 11710
+"""
+#from functools import wraps
+#import json
+#from flask import Flask
+#from flask import make_response
+from Tkinter import *
+
+frmMain = Tk()
+
+label = Label(frmMain, text="Welcome to py2exe!")
+label.pack()
+
+frmMain.mainloop()
